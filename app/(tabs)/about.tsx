e1534e26f233ccdb5fc7580da3949df9a0632ea1
daw_c17 <100421659+dawc17@users.noreply.github.com>
2025-08-19 11:54:12 +0200

    Update About page and add product list
    The About page now displays 'NicTrack, created by dawc17' instead of just 'About'. The Products page has been updated to show a grid of products using FlatList, listing various ZYN, VELO, and Skruf items.

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -4,7 +4,7 @@ import "../../global.css";
 export default function About() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-yellow-500">About</Text>
+      <Text className="text-yellow-500">NicTrack, created by dawc17</Text>
     </View>
   );
 }
--- app/(tabs)/products.tsx
+++ app/(tabs)/products.tsx
@@ -1,10 +1,41 @@
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import "../../global.css";
+
+const DATA = [
+  { id: "1", title: "ZYN Mint" },
+  { id: "2", title: "ZYN Berry" },
+  { id: "3", title: "ZYN Citrus" },
+  { id: "4", title: "ZYN Coffee" },
+  { id: "5", title: "ZYN Mango" },
+  { id: "6", title: "ZYN Vanilla" },
+  { id: "7", title: "VELO Mint" },
+  { id: "8", title: "VELO Berry" },
+  { id: "9", title: "VELO Citrus" },
+  { id: "10", title: "VELO Coffee" },
+  { id: "11", title: "VELO Mango" },
+  { id: "12", title: "VELO Vanilla" },
+  { id: "13", title: "Skruf Fresh" },
+  { id: "14", title: "Skruf Mint" },
+  { id: "15", title: "Skruf Berry" },
+  { id: "16", title: "Skruf Citrus" },
+  { id: "17", title: "Skruf Coffee" },
+  { id: "18", title: "Skruf Mango" },
+  { id: "19", title: "Skruf Vanilla" },
+  { id: "20", title: "Skruf Coffee" },
+];
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-green-500">Products here</Text>
-    </View>
+    <FlatList
+      key={"products-list"}
+      data={DATA}
+      renderItem={({ item }) => (
+        <View className="p-10 border border-gray-200 w-1/2">
+          <Text className="text-lg font-bold text-center">{item.title}</Text>
+        </View>
+      )}
+      keyExtractor={(item) => item.id}
+      numColumns={2}
+    />
   );
 }
